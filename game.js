f7f0346f5d9cdb386bc5fe3601858a68bcb72571
Trish Fox-Collis <tfoxcollis@gmail.com>
2022-04-20 22:27:36 -0700

    Create query selectors with IDs

---
+++ game.js
@@ -0,0 +1,21 @@
+//query selectors
+var playerOneImg = document.querySelector("#playerOneImg");
+var playerTwoImg = document.querySelector("#playerTwoImg");
+var playerOneWins = document.querySelector("#playerOneWins");
+var playerTwoWins = document.querySelector("#playerTwoWins");
+var playerOneQuote = document.querySelector("#playerOneQuote");
+var playerTwoQuote = document.querySelector("#playerTwoQuote");
+var gameTitle = document.querySelector("#gameTitle");
+var tttBox = document.querySelector("#tttBox");
+
+
+
+// eventlisteners
+
+
+
+
+}
+
+
+// functions
